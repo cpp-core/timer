d4870a309b960c3f14319ab4ce4b01c0864febb8
Mark Melton <mark.melton@notlem.com>
2023-06-02 00:10:16 -0400

    WIP/docs

--- include/core/timer/timer.h
+++ include/core/timer/timer.h
@@ -28,12 +28,16 @@ inline void doNotReorderBarrier() {
     std::atomic_signal_fence(std::memory_order_acq_rel);
 }
 
+/// The Timer class template implements a timer designed for minimal
+/// overhead, ad-hoc timing of code regions including micro-timing
+/// down to single machine instructions.
 template<class Duration = std::chrono::nanoseconds,
 	 class Clock = std::chrono::high_resolution_clock>
 class Timer {
 public:
     using TimePoint = typename Clock::time_point;
 
+    /// Run the supplied `code` in a loop `n` times.
     template<class Code>
     Timer& run(size_t n, Code&& code) {
 	start();
@@ -44,10 +48,12 @@ public:
 	return *this;
     }
 
+    /// Start the timer.
     void start() {
 	start_ = Clock::now();
     }
 
+    /// Stop the timer indicating `n` operations.
     auto stop(size_t n = 1) {
 	auto end = Clock::now();
 	iterations_ += n;
@@ -55,10 +61,12 @@ public:
 	return elapsed_;
     }
 
+    /// Return the average number of nanoseconds per operation.
     auto elapsed_per_iteration() const {
 	return iterations_ > 0 ? (double)elapsed_.count() / iterations_ : 0.0;
     }
 
+    /// Return the elapsed duration.
     auto elapsed() const {
 	return elapsed_;
     }
